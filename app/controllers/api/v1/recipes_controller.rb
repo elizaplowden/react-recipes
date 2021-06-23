156281033531b7a074bf378e713a0ebf6c3629f5
Eliza Plowden <elizaplowden@gmail.com>
2021-06-23 11:36:07 +0100

    update and tokens

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::RecipesController < Api::V1::BaseController
-  before_action :set_recipe, only: [:show]
+  acts_as_token_authentication_handler_for User, except: [ :index, :show ]
+  before_action :set_recipe, only: [ :show, :update ]
 
   def index
     @recipes = policy_scope(Recipe)
@@ -8,10 +9,27 @@ class Api::V1::RecipesController < Api::V1::BaseController
   def show
   end
 
+  def update
+    if @recipe.update(recipe_params)
+      render :show
+    else
+      render_error
+    end
+  end
+
   private
 
-  def set_restaurant
-    @restaurant = Restaurant.find(params[:id])
-    authorize @restaurant
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+    authorize @recipe
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :description, :ingredients, :cuisine, :meal, :cook_time, :serves)
+  end
+
+  def render_error
+    render json: { errors: @recipe.errors.full_messages },
+    status: :unprocessable_entity
   end
 end
